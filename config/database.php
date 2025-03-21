542c5898d99c9180298b94098093c7c30aa87605
ivan <danicupacan@gamial.com>
2025-03-21 18:14:57 -0500

    create

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+
+//Config connection 
+$host      = "localhost";
+$port      = "5432";
+$dbname    = "schoolar";
+$user      = "postgres";
+$password  = "unicesmag";
+
+//create connection
+$conn = pg_connect("
+host=$host
+port=$port
+dbname=$dbname
+user=$user
+password=$password
+");
+
+if(!$conn){
+    die("Connection error: " . pg_last_error());
+}else{
+    echo "Success connection";
+}
+?>
