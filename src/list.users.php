abc8570fda361cb5a42c4d3885baeca9ccc69e41
ivan <danicupacan@gamial.com>
2025-05-09 17:21:38 -0500

    home

---
+++ src/list.users.php
@@ -0,0 +1,33 @@
+<?php
+    include('../config/database.php');
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Schoolar - List</title>
+        <link rel="icon" type="image/png" href="icon/batpepe.png">
+    </head>
+    <body>
+        <table border="2">
+        
+            <tr>
+                <td>Firstname</td>
+                <td>Lasttname</td>
+                <td>Email</td>
+                <td>Active</td>
+                <td>
+                    <img src = "icon\edit.png" width="50">
+                    <img src = "icon\pepe_delete.jpg" width="50">
+                    <img src = "icon\edit.png" width="50">
+                </td>
+            </tr>
+            <?php
+                //idk
+            ?>
+
+        </table>
+    </body>
+</html>
---
+++ src/logout.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+session_destroy();
+
+Refresh(':0; url=http://localhost/schoolar/src/signin.html');
+?>
